73882cdd9396e6b6e20300f2d8ef6b1d1414daf5
Shezan <shezanmahmud650@gmail.com>
2022-06-15 00:37:46 +0600

    dynamic card

---
+++ index.js
@@ -0,0 +1,76 @@
+// dummy deta
+
+// const posts = [
+//     {
+//         title : "This is title 1",
+//         body : "This is body 1"
+//     },
+//     {
+//         title : "This is title 2",
+//         body : "This is body 2"
+//     },
+//     {
+//         title : "This is title3",
+//         body : "This is body 3"
+//     },
+//     {
+//         title : "This is title 4",
+//         body : "This is body 4"
+//     },
+//     {
+//         title : "This is title 5",
+//         body : "This is body 5"
+//     },
+//     {
+//         title : "This is title 6",
+//         body : "This is body 6"
+//     },
+//     {
+//         title : "This is title 7",
+//         body : "This is body 7"
+//     },
+//     {
+//         title : "This is title 8",
+//         body : "This is body 8"
+//     }
+// ]
+
+{/* <div class="post">
+            <h4 class="post-title">Post title</h4>
+            <p class="post-body">Post Description 1</p>
+        </div> */}
+
+
+// fatch data
+const fetchData = async (config) => {
+    try{
+        const res = await axios(config);
+        return res.data;
+    }catch(error){
+        throw Error ("data is not fetched")
+    }
+    
+}
+
+
+// selection
+
+const postsElement = document.querySelector('.posts');
+
+
+
+const loadAllDeta =async  () =>{
+    const posts = await fetchData("https://jsonplaceholder.typicode.com/posts");
+    posts.map((post) =>{
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+        postElement.innerHTML = `
+        <h4 class="post-title">Post title ${post.title}</h4>
+        <p class="post-body">${post.body}</p>
+        `;
+        postsElement.appendChild(postElement);
+    });
+    
+};
+
+loadAllDeta();
